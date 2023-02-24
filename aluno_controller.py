2db49597ecba0bec8d2a51330ffa61756b6ccdb2
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-02-24 19:30:59 -0300

    Update aluno_controller.py

--- aluno_controller.py
+++ aluno_controller.py
@@ -1,4 +1,4 @@
-/* amada de controle é responsável por intermediar as requisições enviadas pelo View com as respostas fornecidas pelo Model, 
+/* Camada de controle é responsável por intermediar as requisições enviadas pelo View com as respostas fornecidas pelo Model, 
 processando os dados que o usuário informou e repassando para outras camadas. 
 Numa analogia bem simplista, o controller  operaria como o ‘’maestro de uma orquestra’’  
 que permite a comunicação entre o detentor dos dados e a pessoa com vários questionamentos no MVC. */
