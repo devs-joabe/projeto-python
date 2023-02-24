085e4474315dbe3dc93934b4a54f25e53fab7c8d
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-02-24 19:50:02 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,7 +1,6 @@
-/* Essa classe também é conhecida como Business Object Model (objeto modelo de negócio). 
-Sua responsabilidade é gerenciar e controlar a forma como os dados se comportam por meio das funções, 
-lógica e regras de negócios estabelecidas. */ 
-
+# Essa classe também é conhecida como Business Object Model (objeto modelo de negócio). 
+# Sua responsabilidade é gerenciar e controlar a forma como os dados se comportam por meio das funções, 
+# lógica e regras de negócios estabelecidas.
 
 class Aluno:
     def __init__(self, nome, matricula, curso):
